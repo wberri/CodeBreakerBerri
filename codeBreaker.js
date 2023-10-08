9f8aefdb9bb159b0ea33c05b6304c27d058dcf36
Wafa Berri <wafab@nycstudents.net>
2023-10-08 14:16:08 -0400

    minor changes

--- codeBreaker.js
+++ codeBreaker.js
@@ -7,27 +7,29 @@ var won  = false;
 function testResults(form){
     var userGuess = form.numberGuess.value;
     if (userGuess>randomNum){
-        response.textContent = "Feedback: Your guess is too high." +randomNum;
+        response.textContent = "Feedback: Your guess is too high.";
     } else if (userGuess<randomNum){
-        response.textContent = "Feedback: Your guess is too low." + randomNum;
+        response.textContent = "Feedback: Your guess is too low.";
     } else if (userGuess==randomNum){
-        response.textContent = "Feedback: Correct Guess. You've still got it." + randomNum;
+        response.textContent = "Feedback: Correct Guess. You've still got it.";
         won = true;
     } else{
         response.textContet = "STOP. You have failed.";
     }
     numTries.textContent = numTriesVal-1;
     numTriesVal = numTriesVal-1;
-    if (numTriesVal <= 0 && won!=true){
-        response.textContent = "YOU FAILED. YOU DID NOT GET IT WITHIN 7 TRIES. The number was " + randomNum + ".";
-        numTries.textContent = "FAIL";
-        var userGuess = "TERMINATE";
-    } else if (won==true){
-        response.textContent = "You won!";
-        numTries.textContent = "PASS";
-        var userGuess = "TERMINATE";
-    }
-
+    endgame();
 }
+    function endgame(){
+        if (numTriesVal <= 0 && won!=true){
+            response.textContent = "YOU FAILED. YOU DID NOT GET IT WITHIN 7 TRIES. The number was " + randomNum + ".";
+            numTries.textContent = "FAIL";
+            var userGuess = "TERMINATE";
+        } else if (won==true){
+            response.textContent = "You won! The correct number was " +randomNum+ ".";
+            numTries.textContent = "PASS";
+            var userGuess = "TERMINATE";
+        }
+    }
 
 
